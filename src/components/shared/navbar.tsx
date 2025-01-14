e26a8d37bfe9e2f7c325f8540754e0662ba051e9
leitel <lucaseitel98@gmail.com>
2025-01-14 11:45:58 -0300

    chore: Add navigation menu to navbar component and update package.json

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -1,5 +1,14 @@
 'use client';
 
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
 import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { Button } from '../ui/button';
@@ -7,33 +16,56 @@ import { Button } from '../ui/button';
 const Navbar = () => {
   const session = useSession();
   return (
-    <nav className='flex items-center justify-between w-full h-12 px-12 bg-zinc-300'>
-      <p>Logo</p>
-      {session.data?.user ? (
-        <div className='flex items-center gap-2'>
-          <p>{session.data.user.name}</p>
-          <Button
-            variant='secondary'
-            onClick={() =>
-              signOut({
-                redirect: true,
-                callbackUrl: `${window.location.origin}`,
-              })
-            }>
-            Sign Out
-          </Button>
-        </div>
-      ) : (
-        <div className='flex items-center gap-2'>
-          <Button variant='secondary'>
-            <Link href='/register'>Register</Link>
-          </Button>
-          <Button variant='secondary'>
-            <Link href='/login'>Log in</Link>
-          </Button>
-        </div>
-      )}
-    </nav>
+    <section className='flex items-center justify-between w-full h-14 px-12 bg-zinc-900'>
+      <Button variant='secondary'>
+        <Link href={window.location.origin}>Logo</Link>
+      </Button>
+      <NavigationMenu>
+        <NavigationMenuList>
+          {!session.data?.user && (
+            <>
+              <NavigationMenuItem>
+                <Link href='/register' legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Register
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href='/login' legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Log In
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </>
+          )}
+          {session.data?.user && (
+            <>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className='w-48'>
+                  {session.data.user.name}
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink className='w-48' asChild>
+                    <Button
+                      variant='secondary'
+                      onClick={() =>
+                        signOut({
+                          redirect: true,
+                          callbackUrl: `${window.location.origin}`,
+                        })
+                      }>
+                      Sign Out
+                    </Button>
+                  </NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </>
+          )}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </section>
   );
 };
 
